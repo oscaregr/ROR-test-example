5c871b16be4ebff520035737edeb8f96ba79d2fd
oscaregr <bavuey@hotmail.com>
2023-02-12 19:56:51 -0600

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :authentication do
+    resources :users, only: [:new, :create], path: '/'
+  end
+
+  resources :home, only: [:index, :update, :destroy]
 end
